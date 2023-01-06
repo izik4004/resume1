1b8a4746627566ba97c72ebee80a365f83905038
izik <izik4004@gmail.com>
2023-01-06 17:38:45 +0100

    herosection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import {RiMoonFill} from "react-icons/ri"
 
 const Navbar = () => {
     const navItems = ["Home", "Projects", "About", "content"]
   return (
     <div>
-        <div className='container mx-auto flex justify-between'>
+        <div className='container mx-auto flex justify-between items-center py-4'>
         <div>
-           <h3>IJ</h3>
+           <h3 className="text-5xl font-extrabold">IJ</h3>
         </div>
         <div className='flex gap-16'>
             {navItems.map((item, index)  => (
@@ -14,7 +14,7 @@ const Navbar = () => {
                     <li key={index}>{item}</li>
                 </ul>
             ))}
-            <p>mee</p>
+            <RiMoonFill/>
         </div>
         </div>
     </div>
